ea87050f6769eab46cccadb2502de48dc4731c91
Vincent <vincentk0125@gmail.com>
2020-03-07 14:31:32 +0100

    wip

--- Resources/Private/Assets/TypeScript/templates/PageTree.ts
+++ Resources/Private/Assets/TypeScript/templates/PageTree.ts
@@ -10,7 +10,7 @@ SvgTree.prototype.appendTextElement = function (node) {
     .attr('dx', function (node) {
       return _this.textPosition + (node.locked ? 15 : 0);
     })
-    .attr('dy', 5)
+    .attr('dy', 8)
     .attr('class', 'node-name')
     .on('click', function (node) {
       _this.clickOnLabel(node, this);
@@ -27,16 +27,14 @@ TreeToolbar.prototype.createTemplate = function () {
   var $template = $(
     '<div class="' + _this.settings.toolbarSelector + '">' +
     '<div class="svg-toolbar__menu">' +
-    '<div class="x-btn btn btn-default btn-sm x-btn-noicon" data-tree-show-submenu="filter">' +
-    '<button class="svg-toolbar__btn" data-tree-icon="actions-search" title="' + TYPO3.lang['tree.buttonFilter'] + '"></button>' +
-    '</div>' +
-    '<div class="x-btn btn btn-default btn-sm x-btn-noicon js-svg-refresh">' +
+
     '<button class="svg-toolbar__btn" data-tree-icon="actions-refresh" title="' + TYPO3.lang['labels.refresh'] + '"></button>' +
-    '</div>' +
+
     '</div>' +
     '</div>' +
     '<div class="svg-toolbar__submenu">' +
     '<div class="svg-toolbar__submenu-item" data-tree-submenu="filter">' +
+    '<button class="svg-toolbar__btn" data-tree-icon="actions-search" data-tree-show-submenu="filter" title="' + TYPO3.lang['tree.buttonFilter'] + '"></button>' +
     '<input type="text" class="form-control search-input" placeholder="' + TYPO3.lang['tree.searchTermInfo'] + '">' +
     '</div>' +
     '<div class="svg-toolbar__submenu-item" data-tree-submenu="page-new">' +
@@ -46,10 +44,8 @@ TreeToolbar.prototype.createTemplate = function () {
 
   if (this.tree.settings.doktypes && this.tree.settings.doktypes.length) {
     var $buttons = $template.find('[data-tree-submenu=page-new]');
-    $template.find('.svg-toolbar__menu').prepend('<div class="x-btn btn btn-default btn-sm x-btn-noicon" data-tree-show-submenu="page-new">' +
-      '<button class="svg-toolbar__btn" data-tree-icon="actions-page-new" title="' + TYPO3.lang['tree.buttonNewNode'] + '"></button>' +
-      '</div>'
-    );
+    $template.find('.svg-toolbar__menu').prepend(
+      '<button class="svg-toolbar__btn"  data-tree-show-submenu="page-new" data-tree-icon="actions-page-new" title="' + TYPO3.lang['tree.buttonNewNode'] + '"></button>');
 
     $.each(this.tree.settings.doktypes, function (id, e) {
       _this.tree.fetchIcon(e.icon, false);
