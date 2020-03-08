8a49e2bdaae419c173b8da38de49a8d3d840b8c7
Vincent <vincentk0125@gmail.com>
2020-03-08 04:45:35 +0100

    Add missing package.json

--- Resources/Private/Assets/types/Starfishprime/index.d.ts
+++ Resources/Private/Assets/types/Starfishprime/index.d.ts
@@ -8,3 +8,35 @@ declare namespace Starfishprime {
   export let IconsManager: any;
 
 }
+interface Doktype {
+  nodeType: number;
+  icon: string;
+  title: string;
+  tooltip: string;
+}
+interface TreeNode {
+  hasChildren: boolean;
+  nameSourceField: string;
+  prefix: string;
+  suffix: string;
+  locked: boolean;
+  overlayIcon: string;
+  selectable: boolean;
+  expanded: boolean;
+  checked: boolean;
+  backgroundColor: string;
+  stopPageTree: boolean;
+  class: string;
+  readableRootline: string;
+  isMountPoint: boolean;
+  stateIdentifier: string;
+  identifier: number;
+  depth: number;
+  tip: string;
+  icon:string;
+  name: string;
+  mountPoint: number;
+  workspaceId: number;
+  siblingsCount: number;
+  siblingsPosition: number;
+}
--- Resources/Private/Assets/types/TYPO3/index.d.ts
+++ Resources/Private/Assets/types/TYPO3/index.d.ts
@@ -5,8 +5,56 @@
  * Add types as you use them
  */
 declare namespace TYPO3 {
-  export let Notification: any;
-  export let settings: any;
-  export let langue: any;
-  export let Backend: any;
+    export let Notification: any;
+    export let settings: any;
+    export let langue: any;
+    export let Backend: any;
+}
+
+// type definition for global namespace object
+interface Window {
+    TYPO3: any;
+    $: any; // only required in ImageManipulation.ts
+    startInModule: Array<string>;
+    loadEditId: (id: number, addGetVars?: string) => void;
+    require: Function;
+    list_frame: Window;
+    jump: Function;
+    currentSubScript: string;
+    currentModuleLoaded: string;
+    fsMod: { [key: string]: any };
+    nextLoadModuleUrl: string;
+
+    // required for Paste.ts
+    // TODO these should be passed as data attributes
+    pasteAfterLinkTemplate: string;
+    pasteIntoLinkTemplate: string;
+}
+
+interface JQueryTypedEvent<T extends Event> extends Event {
+    originalEvent: T;
+}
+
+/**
+ * Required to make jQuery plugins "available" in TypeScript
+ */
+interface JQuery {
+    dataTableExt: any;
+
+    search(term?: string): JQuery;
+
+    draw(): JQuery;
+
+    datetimepicker(options?: any): JQuery;
+
+    dragUploader(options?: any): JQuery;
+
+    t3FormEngineFlexFormElement(options?: any): JQuery;
+
+    // To be able to use twbs/bootstrap-slider we have to override the definition of jquerui
+    slider(options: { [key: string]: any }): any;
+
+    // To be able to use jquery/autocomplete-slider we have to override the definition of jquerui
+    autocomplete(options?: { [key: string]: any }): any;
+    disablePagingAction(): void;
 }
