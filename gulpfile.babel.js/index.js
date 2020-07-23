705e6996b042f2b7e3adf105f867093589a09293
Vincent <vincentk0125@gmail.com>
2020-07-23 20:47:41 +0200

    Update gulp file to match themes folders strctuture

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -2,11 +2,11 @@
 'use strict';
 
 import { dest, parallel, series, src, watch as gwatch } from 'gulp';
+import fs from 'fs';
 import notifier from 'gulp-plumber-notifier';
 import stylelint from 'gulp-stylelint';
 import autoprefixer from 'gulp-autoprefixer';
 import sass from 'gulp-sass';
-import babel from 'gulp-babel';
 import eslint from 'gulp-eslint';
 import image from 'gulp-image';
 import ts from 'gulp-typescript';
@@ -15,81 +15,102 @@ import fiber from 'fibers';
 import sourcemaps from 'gulp-sourcemaps';
 import config from './config';
 
-const tsProj = ts.createProject('./tsconfig.json');
-
 // Recognise `--prod` argument
 const argv = yargs.argv;
 const production = !!argv.prod;
 
-export const lintJs = () =>
-    src(config.paths.css.src.map(path => `${path}/**/*.js`))
+const themes = fs.readdirSync(config.paths.src.themes);
+
+const capitalize = s => (typeof s !== 'string' ? '' : s.charAt(0).toUpperCase() + s.slice(1));
+
+const themePath = (theme, type) =>
+    `${config.paths.src.themes}/${theme}/${capitalize(type)}/**/*${type in config.FILES_EXTENSIONS &&
+        '.' + config.FILES_EXTENSIONS[type]}`;
+
+const tsProjects = themes.reduce((acc, theme) => {
+    acc[theme] = ts.createProject('./tsconfig.json', {
+        declaration: true,
+        typeRoots: [themePath(theme, 'types')],
+    });
+    return acc;
+}, {});
+
+const lintTypeScript = theme => {
+    src(themePath(theme, 'TypeScript'))
         .pipe(notifier())
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-
-export const lintScss = () =>
-    src(config.paths.css.src.map(path => `${path}/**/*.scss`)).pipe(stylelint(config.stylelint));
-
-export const optimizeImages = () =>
-    src(config.paths.images.src.map(path => `${path}/**/*`))
-        .pipe(image())
-        .pipe(dest(config.paths.images.dest));
-
-export const optimizeIcons = () =>
-    src(config.paths.icons.src.map(path => `${path}/**/*`))
-        .pipe(image())
-        .pipe(dest(config.paths.icons.dest));
-
-export const buildStyles = () => {
-    series(lintScss);
 };
-export const compileScss = () =>
-    src(config.paths.css.themeSrc.map(path => `${path}/**/*.scss`))
+
+const lintScss = theme => {
+    src(themePath(theme, 'scss')).pipe(stylelint(config.stylelint));
+};
+
+const optimizeImages = theme => {
+    src(themePath(theme, 'Images'))
+        .pipe(image())
+        .pipe(dest(config.paths.dest.images));
+};
+
+const optimizeIcons = theme => {
+    src(themePath(theme, 'Icons'))
+        .pipe(image())
+        .pipe(dest(config.paths.dest.icons));
+};
+
+const compileScss = theme => {
+    src(themePath(theme, 'scss'))
         .pipe(notifier())
         .pipe(sourcemaps.init())
         .pipe(
             sass({
                 fiber: fiber,
-                includePaths: [
-                    'Resources/Private/Assets/Scss',
-                    'node_modules',
-                    'node_modules/bootstrap-sass/assets/stylesheets',
-                    'node_modules/font-awesome/scss',
-                    'node_modules/eonasdan-bootstrap-datetimepicker/src/sass',
-                ],
+                includePaths: config.includePaths,
             }).on('error', sass.logError),
         )
         .pipe(autoprefixer())
         .pipe(sourcemaps.write())
-        .pipe(dest(config.paths.css.dest));
+        .pipe(dest(config.paths.dest.css));
+};
 
-export const compileTypescript = () =>
-    src(config.paths.js.src.map(path => `${path}/**/*.ts`))
-        .pipe(notifier())
+const compileTypescript = theme => {
+    const tsResult = src(themePath(theme, 'TypeScript'))
         .pipe(sourcemaps.init())
-        .pipe(tsProj())
-        .pipe(babel())
-        .pipe(sourcemaps.write())
-        .pipe(dest(config.paths.js.dest));
+        .pipe(tsProjects[theme]());
+    tsResult.js.pipe(sourcemaps.write()).pipe(dest(config.paths.dest.js));
+};
 
 // Watch Task
 export const watch = () => {
     gwatch(
-        [
-            ...config.paths.css.themeSrc.map(path => `${path}/**/*.scss`),
-            ...config.paths.css.cmsSrc.map(path => `${path}/**/*.scss`),
-        ],
-        series(compileScss),
+        themes.map(theme => themePath(theme, 'scss')),
+        path => {
+            return series(compileScss);
+        },
     );
     gwatch(
-        config.paths.js.src.map(path => `${path}/**/*.ts`),
+        themes.map(theme => themePath(theme, 'TypeScript')),
         series(compileTypescript),
     );
 };
 
 // Development Task
-export const dev = parallel(lintJs, lintScss);
+export const dev = parallel(lintTypeScript, lintScss);
+
+const buildThemes = cb => {
+    themes.map(theme => {
+        compileScss(theme);
+        if (theme !== 'Default') {
+            lintScss(theme);
+            lintTypeScript(theme);
+            compileTypescript(theme);
+            optimizeImages(theme);
+            optimizeIcons(theme);
+        }
+    });
+    cb();
+};
 
 // Default task
-export default parallel(compileScss, compileTypescript, optimizeImages, optimizeIcons);
+export default parallel(buildThemes);
